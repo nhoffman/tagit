4055802c601b63a73571dc888d97f4fe5bb974f4
Noah Hoffman <noah.hoffman@gmail.com>
2017-10-20 21:51:00 -0700

    revert to previous method for saving the version string

--- tagit/tagit.py
+++ tagit/tagit.py
@@ -19,40 +19,24 @@ def get_version(datadir=None, version_file='version.txt'):
     # only try to create the version file if setup.py is someplace in the stack
     stack = traceback.extract_stack()
 
-    logfile = open('/Users/nhoffman/src/tagit/stack-{}.txt'.format(hash(str(stack))), 'w')
-    for e in stack:
-        logfile.write(str(e) + '\n')
-
     try:
         in_setup = any(s.filename.endswith('setup.py') for s in stack)
     except AttributeError:
         in_setup = any(s[0].endswith('setup.py') for s in stack)
 
-    logfile.write('in_setup: {}\n'.format(in_setup))
-
     if in_setup:
-        cmd = ['git', 'describe', '--tags', '--dirty']
-
-        try:
-            os.makedirs(datadir)
-        except OSError:
-            pass
-
-        try:
-            git_version = subprocess.check_output(cmd, universal_newlines=True)
-        except Exception as err:
-            sys.stderr.write('error running "{}"\n'.format(' '.join(cmd)))
-            logfile.write(str(err) + '\n')
-        else:
-            version = git_version.strip().replace('-', '+', 1).replace('-', '.')
-            with open(version_file, 'w') as f:
-                f.write(version)
+        sys.stdout.write('updating {} with version '.format(version_file))
+        subprocess.call(
+            ('mkdir -p {datadir} && '
+             'git describe --tags --dirty > {file}.tmp '
+             '&& mv {file}.tmp {file} '
+             '|| rm -f {file}.tmp').format(datadir=datadir, file=version_file),
+            shell=True, stderr=open(os.devnull, "w"))
 
     try:
         with open(version_file) as f:
-            version = f.read()
-    except Exception as err:
-        logfile.write(str(err) + '\n')
+            version = f.read().strip().replace('-', '+', 1).replace('-', '.')
+    except Exception:
         version = ''
 
     return version
